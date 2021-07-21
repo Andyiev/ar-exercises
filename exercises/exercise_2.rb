148078fbd7edc77cc79591a03c1a20d33e09ce59
Andyiev <ayevdokymenmk@gmail.com>
2021-07-20 19:47:45 -0600

    Add exercise_2.rb

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,14 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+@store1 = Store.find(1)
+puts "The store 1 name is #{@store1.name}"
+
+@store2 = Store.find(2)
+puts "The store 2 name is #{@store2.name}"
+
+@store1.name = "Durby"
+puts "The store 1 updated name is #{@store1.name}"
+
+
 # Your code goes here ...
